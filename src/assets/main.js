88e7636d27e5ec57615c922684f218b3e954c48e
Josue Contreras <estavesyano@gmail.com>
2022-11-23 10:03:11 -0500

    Peticion de lista de videos de youtube por medio de una API obtenida en rapidapi.com

---
+++ src/assets/main.js
@@ -0,0 +1,93 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+const content = null || document.getElementById('content');
+
+// const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCMPiPN_ohErOVrJ9wA_Ydwg&part=snippet%2Cid&order=date&maxResults=9';
+// const content = null || document.getElementById('content');
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+        'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+    }
+};
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+//         'X-RapidAPI-Key': '26b2c736camsh95b48327c937b2ep1842e8jsnf6aa57a6c813'
+//     }
+// };
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+// async function fetchData(urlApi) {
+//     const response = await fetch(urlApi, options);
+//     const data = await response.json();
+//     return data;
+// }
+(async () => {
+    try { 
+        const videos = await fetchData(API);
+        let view = `
+            ${videos.items.map(video => `
+                <div class="group relative">
+                    <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-gray-700">
+                            <span aria-hidden="true" class="absolute inset-0"></span>
+                            ${video.snippet.title}
+                        </h3>
+                    </div>
+                </div>
+            `).slice(0,4).join('')}
+        `;
+        content.innerHTML=view;
+    } catch (error) {
+        console.log(error);
+    }
+})();
+// (async () => {
+//     try {
+//         const videos = await fetchData(API);
+//         let view = `
+//             ${videos.items.map(video => `
+//                 <div class="group relative">
+//                     <div
+//                         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+//                         <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+//                     </div>
+//                     <div class="mt-4 flex justify-between">
+//                         <h3 class="text-sm text-gray-700">
+//                             <span aria-hidden="true" class="absolute inset-0"></span>
+//                             ${video.snippet.title}
+//                         </h3>
+//                     </div>
+//                 </div>
+//             `).slice(0,4).join('')}
+//         `;
+//         content.innerHTML=view;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();
+
+
+
+
+
+
+
+
+
+
+
+
